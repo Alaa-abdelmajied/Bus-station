db3887e642f424303a9c5e9f05af0021b9782180
MaramGhazal <48571646+MaramGhazal@users.noreply.github.com>
2019-04-07 13:41:53 +0200

    login file and vipPassengers file updated,Passenger class updated

--- MAP/src/MapLogic/Passenger.java
+++ MAP/src/MapLogic/Passenger.java
@@ -1,5 +1,33 @@
 package MapLogic;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Passenger {
 
+	private ArrayList<String> passengerName = new ArrayList<String>();
+	private ArrayList<Integer> numberOfTrips = new ArrayList<Integer>();
+
+	FileReading loader = new FileReading();
+
+	public Passenger() {
+		passengerName = loader.getPassengerName();
+		numberOfTrips = loader.getNumberOfTrips();
+	}
+
+	public boolean vipCheck(String name) throws IOException {
+
+		loader.readVipFile();
+		for (int i = 0; i < passengerName.size(); i++) {
+			if (name.equals(passengerName.get(i))) {
+				if (numberOfTrips.get(i) >= 6) {
+					return true;
+
+				} else {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
 }
