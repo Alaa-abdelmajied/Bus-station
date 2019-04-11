7b46a262c5c7a14f86d7548f3c347b472c434e13
Alaa-abdelmajied <48594604+Alaa-abdelmajied@users.noreply.github.com>
2019-04-11 15:07:32 +0200

    current and history trips writer

--- MAP/src/MapLogic/FileWriterUtils.java
+++ MAP/src/MapLogic/FileWriterUtils.java
@@ -15,7 +15,7 @@ public class FileWriterUtils {
 		for (int i = 0; i < trip.size(); i++) {
 			writer.write(trip.get(i).getSource() + "," + trip.get(i).getDestination() + "," + trip.get(i).getTime()
 					+ "," + trip.get(i).getVehicle() + "," + trip.get(i).getNumOfStops() + ","
-					+ trip.get(i).getTicketPrice() +"," + trip.get(i).getNumberOfSeats() + "\n");
+					+ trip.get(i).getTicketPrice() + "," + trip.get(i).getNumberOfSeats() + "\n");
 		}
 		writer.close();
 	}
@@ -24,15 +24,54 @@ public class FileWriterUtils {
 
 		ArrayList<Driver> trip = new ArrayList<Driver>();
 		trip.addAll(trips);
-		BufferedWriter writer = new BufferedWriter(new FileWriter("trips.txt"));
+		BufferedWriter writer = new BufferedWriter(new FileWriter("DriverFile"));
 		for (int i = 0; i < trip.size(); i++) {
 			writer.write(trip.get(i).getDriverFirstName() + "," + trip.get(i).getDriverLastName() + ","
 					+ trip.get(i).getDriverGender() + "," + trip.get(i).getDriverNumber());
-			for(int j = 0; i < trip.get(i).getAssignedTrips().size(); j++)
-				writer.write(","+trip.get(i).getAssignedTrips().get(j));
+			for (int j = 0; j < trip.get(i).getAssignedTrips().size(); j++)
+				writer.write("," + trip.get(i).getAssignedTrips().get(j));
 			writer.write("\n");
 		}
 		writer.close();
 	}
-	// current, history//
+
+	public static void writeHistoryTripFile(ArrayList<History> histories) throws IOException {
+
+		ArrayList<History> history = new ArrayList<History>();
+		history.addAll(histories);
+		BufferedWriter writer = new BufferedWriter(new FileWriter("HistoryTrips"));
+		for (int i = 0; i < history.size(); i++) {
+			writer.write(history.get(i).getName());
+			for (int j = 0; j < history.get(i).getTrips().size(); j++) {
+				writer.write("," + history.get(i).getTrips().get(i).getSource() + ","
+						+ history.get(i).getTrips().get(i).getDestination() + ","
+						+ history.get(i).getTrips().get(i).getTime() + ","
+						+ history.get(i).getTrips().get(i).getVehicle() + ","
+						+ history.get(i).getTrips().get(i).getNumOfStops() + ","
+						+ history.get(i).getTrips().get(i).getTicketPrice() + ","
+						+ history.get(i).getTrips().get(i).getNumberOfSeats() + "\n");
+			}
+		}
+		writer.close();
+	}
+	
+	public static void writeCurrentTripFile(ArrayList<Current> currents) throws IOException {
+
+		ArrayList<Current> current = new ArrayList<Current>();
+		current.addAll(currents);
+		BufferedWriter writer = new BufferedWriter(new FileWriter("CurrentTrips"));
+		for (int i = 0; i < current.size(); i++) {
+			writer.write(current.get(i).getName());
+			for (int j = 0; j < current.get(i).getTrips().size(); j++) {
+				writer.write("," + current.get(i).getTrips().get(i).getSource() + ","
+						+ current.get(i).getTrips().get(i).getDestination() + ","
+						+ current.get(i).getTrips().get(i).getTime() + ","
+						+ current.get(i).getTrips().get(i).getVehicle() + ","
+						+ current.get(i).getTrips().get(i).getNumOfStops() + ","
+						+ current.get(i).getTrips().get(i).getTicketPrice() + ","
+						+ current.get(i).getTrips().get(i).getNumberOfSeats() + "\n");
+			}
+		}
+		writer.close();
+	}
 }
